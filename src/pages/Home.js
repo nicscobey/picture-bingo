8751fb802ee0d9fe40255dd68dd0036101de91eb
Nic Scobey <nicscobey@gmail.com>
2022-02-15 23:18:15 -0800

    added second button style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,27 @@
-import './home.css'
+import "./home.css";
 
 const Home = () => {
-    return (
-        <div className="home-page">
-            <div className="hero">
-                <h1 className="hero-text">
-                    FIRSTNAME LASTNAME
-                    <h3 className="hero-title">Software Engineer</h3>  
-                    {/* <div className="learn-more">Learn more</div> */}
-                </h1>
-                <div class="wrap">
-                    <button class="button">Learn More</button>
-                </div>
-            </div>
+  return (
+    <div className="home-page">
+      <div className="hero">
+        <h1 className="hero-text">
+          FIRSTNAME LASTNAME
+          <h3 className="hero-title">Software Engineer</h3>
+          {/* <div className="learn-more">Learn more</div> */}
+        </h1>
+        <div class="wrap">
+          <button class="button">Learn More</button>
         </div>
-    )
-}
+        <a href="#" class="animated-button6">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Button10
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
